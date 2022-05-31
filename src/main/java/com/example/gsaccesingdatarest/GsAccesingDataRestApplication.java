fd69c735749b8901cce1b39a4d137d4dd7aeebf2
Alexis Nasimba <alexis12895@gmail.com>
2022-05-31 08:55:51 +0200

    Initial commit

---
+++ src/main/java/com/example/gsaccesingdatarest/GsAccesingDataRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.gsaccesingdatarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsAccesingDataRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsAccesingDataRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gsaccesingdatarest/GsAccesingDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gsaccesingdatarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsAccesingDataRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
